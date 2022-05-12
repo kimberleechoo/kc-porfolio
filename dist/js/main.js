fd118174dad3848f7efe9b9a274053209dedb3e9
Kimberlee Choo <kimberlee.choo@growthops.asia>
2022-05-12 11:39:23 +0800

    added soy revamp project and enter password function

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,3 +18,52 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Get the modal
+function popUpFunc(){
+  var modal = document.getElementById('popUpModal');
+  const docBody = document.body;
+  modal.style.display = "block";
+  docBody.style.overflow = "hidden"
+
+  
+}
+
+function submitFormButton(form){
+const soyForm = document.getElementById('soyForm');
+const submitForm = document.getElementById('soySubmitButton')
+const submitError = document.getElementById('soyFormErrorMsg')
+const submitEmptyError = document.getElementById('soyFormEmptyMsg')
+ 
+    // console.log('u clicked the submit button', form)
+    const soyPassword = document.getElementById('soyPassword').value
+  
+    if(soyPassword.length !== 0){
+      submitEmptyError.style.display = "none"
+      if(soyPassword === "hellow0rld();"){
+        submitError.style.display = "none"
+        window.location.href="https://www.google.com"
+        return true
+        } else {
+          submitError.style.display = "block"
+          return false
+      }
+    }else {
+      submitEmptyError.style.display = "block"
+      submitError.style.display = "none"
+
+    }
+
+
+}
+
+function closePopUp(){
+  const modalPopUp = document.getElementById('popUpModal')
+  const docBody = document.body;
+  modalPopUp.style.display = "none";
+  docBody.style.overflowY = "scroll"
+}
+
+
+
+
